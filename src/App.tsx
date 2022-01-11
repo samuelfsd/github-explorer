2abe403743d096bb2b5f1552204e83c286097f95
Samuel Alves <samuel.estudo778@gmail.com>
2022-01-11 11:54:42 -0300

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
+const App:React.FC = () => {
   return (
-    <h1>hello world!</h1>
+    <BrowserRouter>
+       <Routes />
+    </BrowserRouter>
   );
 }
 
